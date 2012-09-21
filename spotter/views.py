54a7d97d46b94ef585f757997239c410b00b3238
Simon Willison <simon@lanyrd.com>
2012-09-21 14:42:07 +0100

    /login/ kicks off fb login

--- spotter/views.py
+++ spotter/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from common.shortcuts import render
+from django.conf import settings
+import urllib
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
 
 def spot(request, id):
     return HttpResponse("Spot %s" % id)
@@ -14,4 +16,12 @@ def robots_txt(request):
     )
 
 def channel_html(request):
-	return HttpResponse('<script src="//connect.facebook.net/en_US/all.js"></script>')
+    return HttpResponse('<script src="//connect.facebook.net/en_US/all.js"></script>')
+
+def login(request):
+    fb_login_uri = "https://www.facebook.com/dialog/oauth?" + urlib.urlencode({
+        'client_id': settings.FB_APP_ID,
+        'redirect_uri': 'http://thawing-earth-2731.herokuapp.com/login/done/',
+        'scope': 'email,publish_actions',
+    })
+    return HttpResponseRedirect(fb_login_uri)
